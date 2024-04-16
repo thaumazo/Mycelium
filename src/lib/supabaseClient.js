97848e7e5ea38150f80dc1a6b78e1cda828b9c71
reidpat <61036489+reidpat@users.noreply.github.com>
2024-04-15 18:10:09 -0700

    Basic packages installed (db, css)

---
+++ src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient('https://byiuaepsgxyjorttumfw.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ5aXVhZXBzZ3h5am9ydHR1bWZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMyMjM2NzUsImV4cCI6MjAyODc5OTY3NX0.2GnYSwpiaYux_hIYnSNeFbGWSIYlho2GCxsDnjyWuMU')
---
+++ src/routes/community/+page.svelte
@@ -0,0 +1,30 @@
+<!-- +page.svelte -->
+<script>
+    export let data;
+
+    // Function to extract headers from the first entry
+    let headers = [];
+    if (data.community.length > 0) {
+        headers = Object.keys(data.community[0]);
+    }
+</script>
+
+<table class="table table-zebra">
+    <thead>
+        <tr>
+            {#each headers as header}
+                <th>{header}</th>
+            {/each}
+        </tr>
+    </thead>
+    <tbody>
+        {#each data.community as person}
+            <tr>
+                {#each headers as header}
+                    <td>{person[header]}</td>
+                {/each}
+            </tr>
+        {/each}
+    </tbody>
+</table>
+<!-- end file -->
