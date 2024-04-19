0ec70de6550f0f7df015d61f65963993393fb033
reidpat <61036489+reidpat@users.noreply.github.com>
2024-04-19 10:38:56 -0700

    Changing over to .env variables when connecting to Thaumazo

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,3 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
+import {PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY} from '$env/static/public'
 
-export const supabase = createClient('https://byiuaepsgxyjorttumfw.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ5aXVhZXBzZ3h5am9ydHR1bWZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMyMjM2NzUsImV4cCI6MjAyODc5OTY3NX0.2GnYSwpiaYux_hIYnSNeFbGWSIYlho2GCxsDnjyWuMU')
+export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY)
