1dcc8915c09b594ec295ba3219c6835601fc9105
reidpat <61036489+reidpat@users.noreply.github.com>
2024-04-21 16:39:24 -0700

    Route protection setup (basic, potentially still insecure)

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -5,9 +5,10 @@ import { redirect } from '@sveltejs/kit'
 export const load = (async ({url, locals: { safeGetSession } }) => {
   const { session, user } = await safeGetSession()
 
-  if (!session && url.pathname != '/auth') {
-    // console.log(url)
-    // throw redirect(303, '/auth')
+  //really not sure about this !url.search case here. Right now when it's being redirected from google login without that case it will get stuck in an auth loop. Something isn't loading in the right order. I feel like it should be checking the code somehow perhaps? Or somehow waiting for the session to initialize?
+  if (!session && url.pathname != '/auth' && !url.search) {
+    console.log(url)
+    throw redirect(303, '/auth')
   }
 
   return {
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -5,15 +5,7 @@ import type { PageServerLoad } from './$types'
 export const load: PageServerLoad = async ({ url, locals: { safeGetSession, supabase } }) => {
   const { session } = await safeGetSession()
 
-  const {data: profile} = await supabase
-        .from('profiles')
-        .select('*')
-        .eq('id', session.user.id);
-
-    console.log("profile: ", profile);
-
-  // if the user is already logged in return them to the account page
   
 
-  return { url: url.origin, profile: profile[0] }
+  return { url: url.origin}
 }
