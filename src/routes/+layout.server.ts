ff64643ddaddb49e9cdcd5fa5c854e8156758fbf
reidpat <61036489+reidpat@users.noreply.github.com>
2024-04-21 19:48:21 -0700

    rolling back auth protection due to auth loop bug. TODO fix soon.

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -7,8 +7,8 @@ export const load = (async ({url, locals: { safeGetSession } }) => {
 
   //really not sure about this !url.search case here. Right now when it's being redirected from google login without that case it will get stuck in an auth loop. Something isn't loading in the right order. I feel like it should be checking the code somehow perhaps? Or somehow waiting for the session to initialize?
   if (!session && url.pathname != '/auth' && !url.search) {
-    console.log(url)
-    throw redirect(303, '/auth')
+    console.log("TODO: redirect here for security. Currently getting caught in auth loop")
+    // throw redirect(303, '/auth')
   }
 
   return {
