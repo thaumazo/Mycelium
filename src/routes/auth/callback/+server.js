d8931cc48fe735967446cf790a794a8bae8e463f
reidpat <61036489+reidpat@users.noreply.github.com>
2024-04-28 12:13:30 -0700

    switching to personal

--- src/routes/auth/callback/+server.js
+++ src/routes/auth/callback/+server.js
@@ -5,7 +5,12 @@ export const GET = async ({ url, locals: { supabase } }) => {
   const code = url.searchParams.get('code')
 
   if (code) {
-    await supabase.auth.exchangeCodeForSession(code)
+    try {
+      await supabase.auth.exchangeCodeForSession(code)  
+    } catch (error) {
+       console.log(error)
+    }
+    
   }
   else {
     throw redirect(303, '/auth')
