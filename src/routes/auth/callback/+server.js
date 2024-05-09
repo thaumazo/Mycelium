d304a875133172a660aba6a52ab60cc52dea89a1
reidpat <61036489+reidpat@users.noreply.github.com>
2024-05-09 14:09:57 -0700

    More attempts at getting google auth functional without errors

--- src/routes/auth/callback/+server.js
+++ src/routes/auth/callback/+server.js
@@ -1,13 +1,21 @@
 // src/routes/auth/callback/+server.js
 import { redirect } from '@sveltejs/kit'
 
-export const GET = async ({ url, locals: { supabase } }) => {
+export const GET = async ({ url, locals: { supabase, safeGetSession } }) => {
   const code = url.searchParams.get('code')
+  const session = await safeGetSession();
 
   if (code) {
     try {
       await supabase.auth.exchangeCodeForSession(code)
-  } catch (error) {}
+  } catch (error) {
+    console.log(error)
+  }
+  }
+  else if(session)
+    throw redirect(303, '/')
+  else {
+    throw redirect(303, '/auth')
   }
 
   throw redirect(303, '/')
