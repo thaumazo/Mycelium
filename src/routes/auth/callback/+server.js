e04b23fe3c6f009fe9f9d0ccc20187f302d92a2d
reidpat <61036489+reidpat@users.noreply.github.com>
2024-05-09 15:25:16 -0700

    trying one more time...

--- src/routes/auth/callback/+server.js
+++ src/routes/auth/callback/+server.js
@@ -1,25 +1,18 @@
 // src/routes/auth/callback/+server.js
-import { redirect, error } from '@sveltejs/kit'
+import { redirect } from '@sveltejs/kit'
 
 export const GET = async ({ url, locals: { supabase } }) => {
   const code = url.searchParams.get('code')
 
   if (code) {
     try {
-      const { error: authError } = await supabase.auth.exchangeCodeForSession(code)
-
-      if (authError) {
-        console.error('Error exchanging code:', authError.message)
-        throw error(500, authError.message)
-      }
-
-      throw redirect(303, '/')
-    } catch (err) {
-      console.error('Unexpected error during the callback:', err)
-      throw error(500, err.message)
-    }
-  } else {
-    console.error('Missing auth code in query parameters')
+      await supabase.auth.exchangeCodeForSession(code)
+  } catch (error) {}
+  }
+  else {
     throw redirect(303, '/auth')
   }
+
+  throw redirect(303, '/')
+
 }
