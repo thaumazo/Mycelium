444ff037975655ee065176e1635cd3d39ec0dfdc
reidpat <61036489+reidpat@users.noreply.github.com>
2024-05-09 15:21:34 -0700

    Create GenericCard.svelte

---
+++ src/lib/components/GenericCard.svelte
@@ -0,0 +1,10 @@
+<script>
+	export let title;
+</script>
+
+<div class="card w-96 bg-neutral text-neutral-content">
+	<div class="card-body items-center text-center">
+		<h1 class="text-2xl">{title}</h1>
+		<slot />
+	</div>
+</div>
