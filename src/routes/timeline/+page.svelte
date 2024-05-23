bd4b31f773abb36718df14ff8776ee2f7642667d
reidpat <61036489+reidpat@users.noreply.github.com>
2024-05-22 18:32:58 -0700

    Fixing Supabase join query error

--- src/routes/timeline/+page.svelte
+++ src/routes/timeline/+page.svelte
@@ -4,24 +4,30 @@
 
 	// Function to extract headers from the first entry
 	let headers = [];
-	let log = []
-	$: {if (data.data.length > 0) {
-		console.log(data);
-		
-		data.data.forEach(element => {
-			let tags = []
-			element.item.tags.forEach(tag => {
-				tags.push(tag.name);
-			})
-			log.push({created_at: new Date(element.created_at).toLocaleString(), item: element.item.name, tags: tags, description: element.description})
-		});
-		headers = Object.keys(log[0]);
-		console.log(log);
-	}}
+	let log = [];
+	$: {
+		if (data.data.length > 0) {
+			console.log(data);
 
-	
-
-
+			data.data.forEach((element) => {
+				let tags = [];
+				console.log(element);
+				if (element.item.tags) {
+					element.item.tags.forEach((tag) => {
+						tags.push(tag.name);
+					});
+				}
+				log.push({
+					created_at: new Date(element.created_at).toLocaleString(),
+					item: element.item.name,
+					tags: tags,
+					description: element.description
+				});
+			});
+			headers = Object.keys(log[0]);
+			console.log(log);
+		}
+	}
 
 	import GenericTable from '$lib/components/GenericTable.svelte';
 </script>
@@ -29,7 +35,5 @@
 {#if data.data}
 	<GenericTable {headers} data={log} />
 {/if}
-
-
 
 <!-- end file -->
