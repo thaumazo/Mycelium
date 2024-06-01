72cdcac8da32f973e0dc5dbc66b0c7637680ce52
reidpat <61036489+reidpat@users.noreply.github.com>
2024-06-01 09:53:23 -0700

    Changing limit for num items displayed on timeline

--- src/routes/timeline/+page.server.js
+++ src/routes/timeline/+page.server.js
@@ -5,7 +5,7 @@ export async function load(event) {
   // Define searchParams as an object with default parameters
   let filter = {
     select: "id, created_at, item(id, name, tags(name)), description", // ex: id, name, profile!inner(id, name) 
-    limit: 10,
+    limit: 20,
     order: "created_at.desc"
     // Add additional query parameters as needed, e.g., eq: "id=value", "name=test"
   };
